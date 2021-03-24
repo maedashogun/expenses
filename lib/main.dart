587e7965d94137563c0b3944ca341189be1e85f9
Luciano Araújo Maeda <maedahikaro@gmail.com>
2021-03-24 19:27:09 -0300

    Atualização do projeto

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,31 @@ main() => runApp(ExpensesApp());
 class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyhomePage());
+    return MaterialApp(home: MyHomePage());
   }
 }
 
-class MyhomePage extends StatelessWidget {
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Despesas Pessoais'),
+        title: Text('Depesas Pessoais'),
       ),
-      body: Center(
-        child: Text('Versão Inicial'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('Lista de Transações'),
+          ),
+        ],
       ),
     );
   }
